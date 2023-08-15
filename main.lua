4b12ad8ab77b7c44fc25a1444f5ab776cccf5459
DavidWeaverAudio <78392269+DavidWeaverAudio@users.noreply.github.com>
2023-08-15 01:05:28 +0000

    Update

--- main.lua
+++ main.lua
@@ -1,98 +1,95 @@
 local rooms = {
-    introduction = {
-        description = "You find yourself in the village of Barovia...",
-        exits = {north = "houseEntrance"},
-         
-        items = {
-            sign = {
-                description = "The sign reads: 'Welcome to Barovia.'"
-            }
-        }
-    },
-    houseEntrance = {
-        description = "You stand before the Durst Mansion...",
-        exits = {south = "introduction", north = "livingRoom"},
-        -- ... add more items
-    },
-    livingRoom = {
-        description = "You're in a spooky house. What more can I say?",
-    exits = {north = "houseEntrance"},
+  introduction = {
+    description = "You find yourself in the village of Barovia...",
+    exits = { north = "houseEntrance" },
+
+    items = {
+      sign = {
+        description = "The sign reads: 'Welcome to Barovia.'"
+      }
+    }
+  },
+  houseEntrance = {
+    description = "You stand before the Durst Mansion...",
+    exits = { south = "introduction", north = "livingRoom" },
+    -- ... items
+  },
+  livingRoom = {
+    description = "You're in a spooky house. What more can I say?",
+    exits = { north = "houseEntrance" },
     items = {
       doll = {
         description = "A small pink doll"
       }
     }
-    }
-    -- ... add more rooms
+  }
+  -- ... rooms
 }
 
--- Define player
+-- player
 local player = {
-    currentRoom = "introduction",
-    inventory = {},
+  currentRoom = "introduction",
+  inventory = {},
 }
 
 local actions = {
-    go = function(direction)
-        local newRoom = rooms[player.currentRoom].exits[direction]
-        if newRoom then
-            player.currentRoom = newRoom
-        else
-            print("You can't go that way!")
-        end
-    end,
-    inspect = function(item)
-        if rooms[player.currentRoom].items and rooms[player.currentRoom].items[item] then
-            print(rooms[player.currentRoom].items[item].description)
-        else
-            print("You don't see that here.")
-        end
-    end,
-  take = function(item)
-        if rooms[player.currentRoom].items and rooms[player.currentRoom].items[item] then
-            table.insert(player.inventory, item)
-            print("You took the " .. item .. ".")
-            rooms[player.currentRoom].items[item] = nil
-        else
-            print("You can't take that.")
-        end
-    end,
-
-    inventory = function()
-        if #player.inventory == 0 then
-            print("You have no items.")
-        else
-            print("You have:")
-            for _, item in ipairs(player.inventory) do
-                print(item)
-            end
-        end
+  go = function(direction)
+    local newRoom = rooms[player.currentRoom].exits[direction]
+    if newRoom then
+      player.currentRoom = newRoom
+    else
+      print("You can't go that way!")
     end
+  end,
+  inspect = function(item)
+    if rooms[player.currentRoom].items and rooms[player.currentRoom].items[item] then
+      print(rooms[player.currentRoom].items[item].description)
+    else
+      print("You don't see that here.")
+    end
+  end,
+  take = function(item)
+    if rooms[player.currentRoom].items and rooms[player.currentRoom].items[item] then
+      table.insert(player.inventory, item)
+      print("You took the " .. item .. ".")
+      rooms[player.currentRoom].items[item] = nil
+    else
+      print("You can't take that.")
+    end
+  end,
+
+  inventory = function()
+    if #player.inventory == 0 then
+      print("You have no items.")
+    else
+      print("You have:")
+      for _, item in ipairs(player.inventory) do
+        print(item)
+      end
+    end
+  end
 }
 
 local function getRoomDescription(room)
-    local description = room.description
-    if room.items then
-        for item, details in pairs(room.items) do
-            description = description .. "\nYou see a " .. item .. " here."
-        end
+  local description = room.description
+  if room.items then
+    for item, details in pairs(room.items) do
+      description = description .. "\nYou see a " .. item .. " here."
     end
-    return description
+  end
+  return description
 end
 
 -- Game loop
 while true do
-    -- Display room description
-    print(getRoomDescription(rooms[player.currentRoom]))
-    
-    -- Get player input
-    print("\nWhat do you want to do?")
-    local input = io.read()
-    local action, argument = input:match("^(%w+)%s*(%w*)$")
+  print(getRoomDescription(rooms[player.currentRoom]))
+  print("\nWhat do you want to do?")
+  local input = io.read()
+  local action, argument = input:match("^(%w+)%s*(%w*)$")
 
-    if actions[action] then
-        actions[action](argument)
-    else
-        print("I don't understand that command.")
-    end
+  if actions[action] then
+    actions[action](argument)
+  else
+    print("I don't understand that command.")
+  end
 end
